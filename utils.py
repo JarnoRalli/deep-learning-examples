39d565fe46ddd391ffcc8bdf0697a47378617bea
Jarno Ralli <jarno@ralli.fi>
2022-03-12 11:21:06 +0200

    Added an example regarding TensorFlow 2 Object Detection API and SSD object detection

--- utils.py
+++ utils.py
@@ -7,14 +7,32 @@ import numpy as np
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 import itertools
+from PIL import Image
 
 def get_confusion_matrix(model : Model, generator : ImageDataGenerator, data_path : os.path, image_size, batch_size=32):
-    '''
+    '''Generates confusion matrix based based on images in sub-directories in a given path.
+
     Creates a confusion matrix based on an ImageDataGenerator with flow_from_directory.
     This method iterates through the images stored in directories (each sub-directory
     contains images from a particular class) and predicts, for each image, using the model,
     which class the input images belongs to. Generates a confusion matrix that can be
     used to assess how good the model is.
+    
+    Args:
+        model::tensorflow.keras.models.Model
+            NN model that predicts the class on an image
+        generator::tensorflow.keras.preprocessing.image.ImageDataGenerator
+            TF ImageDataGenerator for traversing the images
+        data_pata::os.path
+            path to the directory where the sub-directories containing the images are
+        image_size::
+            size of the images fed into the NN
+        batch_size::int
+            batch size of images
+
+    Returns:
+        (cm::np.array, class_names::[str])
+            A tuple containing the confusion matrix and a list of the class names
     '''
     print('Creating a confusion matrix')
     predictions = []
@@ -38,15 +56,26 @@ def get_confusion_matrix(model : Model, generator : ImageDataGenerator, data_pat
     cm = confusion_matrix(targets, predictions)
     return cm, list(generator_flow.class_indices.keys())
 
-# Plot confusion matrix
-def plot_confusion_matrix(cm, classes,
-                          title='Confusion matrix',
-                          normalize=False,
-                          cmap=plt.cm.Blues):
-    '''
+def plot_confusion_matrix(  cm, 
+                            classes,
+                            title='Confusion matrix',
+                            normalize=False,
+                            cmap=plt.cm.Blues):
+    '''Plots confusion matrix.
+    
     This function prints and plots the confusion matrix.
     Normalization can be applied by setting `normalize=True`.
     Source: https://github.com/lazyprogrammer/machine_learning_examples/blob/master/cnn_class2/util.py
+    
+    Args:
+        cm::np.array
+            confusion matrix
+        classes::[str]
+            names of the classes
+        normalize::bool(False)
+            normalize the numbers to be between 0..1
+        cmap::matplotlib.pyplot.plt.cm(matplotlib.pyplot.plt.cm.Blues)
+            color map
     '''
     if normalize:
         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
@@ -69,3 +98,16 @@ def plot_confusion_matrix(cm, classes,
     plt.xlabel('Predicted label')
     plt.show()
 
+def load_image(path):
+    '''Loads image from a file into a numpy array.
+    
+    Args:
+        path::
+            path to the image to be loaded
+
+    Returns::
+        image::np.array
+            uint8 numpy array with shape (height, width, 3)
+    '''
+
+    return np.array(Image.open(path))
